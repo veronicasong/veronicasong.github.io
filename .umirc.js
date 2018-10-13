3331e59dec6deca73d533ad182587916b8cf9e40
focus7eleven <441002295@qq.com>
2018-10-14 05:11:35 +0800

    remove hash in url

--- .umirc.js
+++ .umirc.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 export default {
-  history: 'hash',
+  // history: 'hash',
   alias: {
     'services': path.resolve(__dirname, 'src/services/'),
     'decorators': path.resolve(__dirname, 'src/decorators/'),
